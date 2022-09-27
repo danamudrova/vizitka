3e1ace1ab596021b62665a14023e3ce9263ac7c0
Dana <danamudrova@gmail.com>
2022-09-27 20:55:29 +0200

    ukol 2

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
-const rodneCislo = prompt('zadejte rodné číslo')
 
+const rodnaCislaKOtestovani = [
+    '123',
+    'nepovím',
+    '7060201236',
+    '123456789123456789',
+    '9062185440',
+    '123č56q8y7',
+  ];
+
+rodnaCislaKOtestovani.forEach(element=> checkBirthID(element) )
+
+
+function checkBirthID (rodneCislo){
 let pocitadlo = 0
-
-function desitka () {
-
 if (rodneCislo.length===10) {
-    console.log('Zadané rodné číslo má správně deset znaků.')
     pocitadlo = 1
 } else {
-    console.log ('Uživatel zadal rodné číslo neplatné délky.')
+    console.log ('invalidLength')
 } 
-}
 
-
- function celeCislo() {
 if (Number.isInteger(Number(rodneCislo))===true) {
-    console.log ('rodne cislo je cele')
     pocitadlo = 2
 
 } else {
-    console.log('chyba')
-}
+    console.log('notANumber')
 }
 
-function delitelnost(){
- 
 if (rodneCislo%11===0) {
-    console.log('je dělitelné 11')
-    pocitadlo = 3
+       pocitadlo = 3
 } else {
-    console.log ('není dělitelné 11')
+    console.log ('failedChecksum')
 } 
-}
 
-function platnost(){
-    if (pocitadlo>=3) {
-        console.log('rodne cislo')
-    } else {
-        console.log('neplatne')
-    }
+
+if (pocitadlo>=3) {
+        console.log('ok')
+} 
     
 }
 
-desitka()
-celeCislo()
-delitelnost()
-platnost()
+
